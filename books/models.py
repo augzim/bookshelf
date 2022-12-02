906fab731982bf91d237281ff8b2f4f52e063fc5
AugZim <augustzim93@gmail.com>
2022-12-02 12:39:46 +0400

    Create all models in 'books' app, fix typo
    Typo was found in bookshelf/settings.py in the default argument name of
    LANGUAGE_CODE variable.

--- books/models.py
+++ books/models.py
@@ -1,3 +1,91 @@
+from datetime import date, timedelta
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core import validators
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey('Author', on_delete=models.PROTECT)
+    pages = models.PositiveSmallIntegerField()
+    genre = models.ManyToManyField('Genre')
+    publisher = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000, )
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name='amount')
+
+    LANGUAGES = (
+        ('en', 'English'),
+        ('fr', 'French'),
+        ('de', 'German'),
+        ('el', 'Greek'),
+        ('it', 'Italian'),
+        ('ru', 'Russian'),
+        ('es', 'Spanish'),
+    )
+
+    language = models.CharField(max_length=2, choices=LANGUAGES, default='en')
+
+    COVERS = (
+        (0, 'Softcover'),
+        (1, 'Hardcover Case Wrap'),
+        (2, 'Hardcover Dust Jacket'),
+    )
+
+    cover = models.PositiveSmallIntegerField(choices=COVERS, verbose_name='Book Cover')
+
+    isbn = models.CharField(
+        max_length=17,
+        verbose_name='ISBN-10/13',
+        help_text='Correct ISBN format: (XXX-)X-XXXXXX-XX-X.\nIf prefix exists remove parenthesis',
+        db_index=True,
+        validators=[
+            validators.RegexValidator(
+                regex=r'(?:\d{3}-)?\d-\d{6}-\d{2}-\d',
+                message='Please, see correct ISBN format above',
+            )
+        ],
+    )
+
+    def __str__(self):
+        return self.title
+
+    def is_available(self):
+        return f'{self.title} ({self.isbn}) is AVAILABLE' if self.quantity \
+                else f'{self.title} ({self.isbn}) is NOT currently AVAILABLE. ' \
+                     f'Expected time of receipt: {str(date.today() + timedelta(days=14))}'
+
+    def get_absolute_url(self):
+        return f'/books/{self.pk}/'
+
+    class Meta:
+        ordering = ['title', ]
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    description = models.TextField(max_length=5000, blank=True)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    MARKS = (
+        (1, 'awful'),
+        (2, 'bad'),
+        (3, 'average'),
+        (4, 'good'),
+        (5, 'excellent'),
+    )
+
+    mark = models.PositiveSmallIntegerField(choices=MARKS, verbose_name='Rating')
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, verbose_name='Country', blank=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
--- bookshelf_project/settings.py
+++ bookshelf_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = config('LANGUAGE_CODE', defualt='en-us')
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
 
 TIME_ZONE = config('TIME_ZONE', default='UTC')
 
